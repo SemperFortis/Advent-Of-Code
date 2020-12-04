0f6afd409bf2dccb2093f45c4c23a6315b8f74ab
Jason <52555403+SemperFortis@users.noreply.github.com>
2020-12-03 21:25:33 -0500

    fix typo

--- Solutions/DayThree.cs
+++ Solutions/DayThree.cs
@@ -10,7 +10,7 @@ namespace Solutions
             string[] map = File.ReadAllText(Path.Combine(Environment.CurrentDirectory, "..\\..\\..\\Solutions\\Inputs\\DayThree.txt")).Split("\n");
 
             Console.WriteLine($"Part one solution: {Solve(map)}");
-            Console.WriteLine($"Part one solution: {Solve(map, 1, 1) * Solve(map) * Solve(map, 5, 1) * Solve(map, 7, 1) * Solve(map, 1, 2)}");
+            Console.WriteLine($"Part two solution: {Solve(map, 1, 1) * Solve(map) * Solve(map, 5, 1) * Solve(map, 7, 1) * Solve(map, 1, 2)}");
         }
 
         private static int Solve(string[] map, int across = 3, int down = 1)
@@ -24,7 +24,7 @@ namespace Solutions
                 row += down;
 
                 if (index + across < map[row].Length) index += across;
-                else index = (index + across - map[row].Length);
+                else index = index + across - map[row].Length;
 
                 if (map[row][index] == '#') trees++;
             }
